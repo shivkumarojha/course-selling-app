d171a6646f1095b5653d2d15bd62e2d9847954e5
shivkumarojha <ojhashivkumar88@gmail.com>
2024-02-02 19:59:28 +0530

    Basic layout of files done with react router

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,6 @@
+function LoginPage() {
+    return (
+        <h1>Login page</h1>
+    )
+}
+export default LoginPage
---
+++ src/components/SignupPage.jsx
@@ -0,0 +1,5 @@
+export default function SignupPage() {
+    return (
+        <h1>SignUp</h1>
+    )
+}
